107de27f5c735ff3e8c25c906f3083f30cda4ec5
mochammadsufandi <mochammaddwi09@gmail.com>
2025-02-04 13:48:30 +0700

    deploy server

---
+++ prisma/seed.ts
@@ -0,0 +1,26 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const satuanKerja = [
+  { nama: "Kejaksaan Tinggi Jambi" },
+  { nama: "Kejaksaan Negeri Jambi" },
+  { nama: "Kejaksaan Negeri Batanghari" },
+  { nama: "Kejaksaan Negeri Bungo" },
+  { nama: "Kejaksaan Negeri Sungai Penuh" },
+  { nama: "Kejaksaan Negeri Merangin" },
+  { nama: "Kejaksaan Negeri Tanjung Jabung Barat" },
+  { nama: "Kejaksaan Negeri Sarolangun" },
+  { nama: "Kejaksaan Negeri Tebo" },
+  { nama: "Kejaksaan Negeri Muaro Jambi" },
+  { nama: "Kejaksaan Negeri Tanjung Jabung Timur" },
+  { nama: "Cabjari Batanghari Muara Tembesi" },
+];
+
+async function seedSatuanKerja() {
+  await prisma.unitKerja.createMany({
+    data: satuanKerja,
+  });
+}
+
+seedSatuanKerja();
